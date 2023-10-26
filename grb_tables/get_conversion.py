c27dc95bc2e82609012a3568aa8ec0d9531d10e8
Adam J. Hennessy <ah724@leicester.ac.uk>
2023-10-26 15:34:31 +0100

    seconsd commit

--- grb_tables/get_conversion.py
+++ grb_tables/get_conversion.py
@@ -4,6 +4,7 @@ import requests
 import re
 import pandas as pd
 import numpy as np
+import concurrent.futures
 
 logger = logging.getLogger('get_tables')
 logger.setLevel('INFO')
@@ -12,29 +13,28 @@ formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
 handler.setFormatter(formatter)
 logger.addHandler(handler)
 
-data_filepath = 'grb_tables/all_grb.csv'
+def scrape_and_update(row):
 
-data = pd.read_table(data_filepath, sep=',', header=0)
-data['conversion'] = np.nan
-
-for idx, row in data.iterrows():
+    logger.info(f"Starting for {int(row.Trig_ID)}")
 
     try:
         trig_id = int(row.Trig_ID)
     except:
-        logger.critical(f"Failed for {row.Trig_ID}")
+        logger.debug(f"FAILED - Can't make int for {row.Trig_ID}")
 
-    logger.info(f"Scraping for {trig_id}...")
     url = f"https://www.swift.ac.uk/xrt_curves/{trig_id}/"
-
-    try:
-        response = requests.get(url)
-        soup = BeautifulSoup(response.content, 'html.parser')
-        fluxcurve_div = soup.find('div', {'id': 'fluxCurve'})
-        conversion_line = fluxcurve_div.find('p').text.strip()
-    except Exception as e:
-        logger.critical(f"Fail: {e}")
-        continue
+    response = requests.get(url)
+    if response.status_code == 200:
+        try:
+            soup = BeautifulSoup(response.content, 'html.parser')
+            fluxcurve_div = soup.find('div', {'id': 'fluxCurve'})
+            conversion_line = fluxcurve_div.find('p').text.strip()
+        except:
+            logger.debug(f"FAILED {trig_id} - no value on webpage.")
+    elif response.status_code == 404:
+        logger.debug(f"FAILED {trig_id} - no lightcurve for this data.")
+    else:
+        logger.debug(f"FAILED {trig_id} -failed with error {response.status_code}.")
     
     ## Grab the correct values from line.
     pattern = r'(\d+\.\d+) x 10-(\d+)'
@@ -42,10 +42,26 @@ for idx, row in data.iterrows():
     A, B = float(match.group(1)), int(match.group(2))
     conversion_factor = A * 10 ** -B
 
-    data.loc[data.index[idx], 'conversion'] = conversion_factor
+    try:
+        data.loc[data[data['Trig_ID'] == trig_id].index, 'conversion'] = conversion_factor
+    except:
+        logger.debug(f"FAILED {trig_id} - something wrong with assigned data.")
+    
+    if conversion_factor is float and conversion_factor > 0:
+        logger.info(f"Success for {trig_id} - {conversion_factor}")
+    else:
+        logger.warning(f"Failed for {trig_id}")
 
-    logger.info(f"Success.")
+if __name__ == '__main__':
 
-logger.info(f"Finishing!")
-data.to_csv('final.csv', index=False)
+    data_filepath = 'grb_tables/all_grb.csv'
+
+    data = pd.read_table(data_filepath, sep=',', header=0)
+    data['conversion'] = np.nan
+
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        executor.map(scrape_and_update, data.itertuples(index=False))
+
+    logger.info(f"Finishing!")
+    data.to_csv('grb_tables/all_grb.csv', index=False)
 
